c46545e09f8d13a60a06cc1e39eca6dd2f6ee3be
Franklin Esquivel <franklin.esquivel@outlook.com>
2018-08-26 15:29:51 -0600

    Client Menu - Confirmation Mail
     - Fixing client menu items and icons
     - Fixing confirmation mail URL.

--- src/main/java/sv/edu/udb/www/controllers/userController.java
+++ src/main/java/sv/edu/udb/www/controllers/userController.java
@@ -214,7 +214,7 @@ public class userController extends HttpServlet {
             user.setIdConfirmation(UserModel.getIdConfirmation());
 
             Mail gmail = new Mail();
-            String url = request.getRequestURL().toString() + "?op=confirmation&id=" + user.getIdConfirmation();
+            String url = request.getServerName() + ":" + request.getServerPort() + request.getContextPath() + "/user.do?op=confirmation&id=" + user.getIdConfirmation();
             gmail.setAddressee(user.getEmail());
             gmail.setAffair("Bienvenido a la cuponera");
             gmail.setMessage("Bienvenido usuario <h3>" + user.getName() + " " + user.getLastName() + "</h3>"
@@ -315,7 +315,7 @@ public class userController extends HttpServlet {
 
             user.setIdConfirmation(UserModel.getIdConfirmation());
             Mail gmail = new Mail();
-            String url = request.getRequestURL().toString() + "?op=confirmation&id=" + user.getIdConfirmation();
+            String url = request.getServerName() + ":" + request.getServerPort() + request.getContextPath() +  "/user.do?op=confirmation&id=" + user.getIdConfirmation();
 
             gmail.setAddressee(user.getEmail());
             gmail.setAffair("Bienvenido a la cuponera");
